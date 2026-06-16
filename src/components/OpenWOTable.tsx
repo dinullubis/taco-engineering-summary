a6fb40e9addb765088611b60e652faabfe3a59f0
dinullubis <ahmad.dinul@taco.co.id>
2026-06-16 17:29:16 +0700

    Update OpenWOTable.tsx

--- src/components/OpenWOTable.tsx
+++ src/components/OpenWOTable.tsx
@@ -1,1 +1,13 @@
+function TopBreakdownTable() {
 
+  return (
+    <div className="bg-[#1E293B] rounded-xl p-5">
+      <h3 className="text-white text-lg font-semibold">
+        Top Breakdown Machine
+      </h3>
+    </div>
+  );
+
+}
+
+export default TopBreakdownTable;
