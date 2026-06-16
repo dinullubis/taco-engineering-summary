17b81c2aa96d0ae0f32aa27d2f2740763c5494a4
dinullubis <ahmad.dinul@taco.co.id>
2026-06-16 17:29:58 +0700

    Update TopBreakdownTable.tsx

--- src/components/TopBreakdownTable.tsx
+++ src/components/TopBreakdownTable.tsx
@@ -1,18 +1,12 @@
-function TopBreakdownTable(){
+function TopBreakdownTable() {
 
-return(
-
-<div className="bg-[#1E293B] rounded-xl p-5">
-
-<h3 className="text-white">
-
-Top Breakdown Machine
-
-</h3>
-
-</div>
-
-)
+  return (
+    <div className="bg-[#1E293B] rounded-xl p-5">
+      <h3 className="text-white text-lg font-semibold">
+        Top Breakdown Machine
+      </h3>
+    </div>
+  );
 
 }
 
