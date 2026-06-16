4106add110362d7be222628466409d2fcbb849f0
dinullubis <ahmad.dinul@taco.co.id>
2026-06-16 16:26:24 +0700

    Create BreakdownChart.tsx

---
+++ src/components/BreakdownChart.tsx
@@ -0,0 +1,25 @@
+function BreakdownChart(){
+
+return(
+
+<div className="bg-[#1E293B] rounded-xl p-5 border border-slate-700 h-[350px]">
+
+<h3 className="text-white text-xl font-semibold">
+
+Breakdown by Area
+
+</h3>
+
+<div className="mt-8 text-slate-400">
+
+Chart akan membaca DATABASE_WO
+
+</div>
+
+</div>
+
+)
+
+}
+
+export default BreakdownChart;
