7a3e97cc6b452e1d5367bb9cc1d146f95dd30f6f
dinullubis <ahmad.dinul@taco.co.id>
2026-06-16 16:25:59 +0700

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -22,55 +22,60 @@ Engineering KPI & Maintenance Monitoring
 
 
 
-<div className="grid grid-cols-4 gap-6 mt-8">
-
+<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-8">
 
 <KPIcard
-
 title="Attendance Rate"
-
 value="96.5"
-
 unit="%"
-
 />
 
-
 <KPIcard
-
 title="OT Hours"
-
 value="145"
-
 unit="hrs"
-
 color="text-orange-400"
-
 />
 
-
 <KPIcard
-
 title="WO Close"
-
 value="87"
-
 />
-
 
 <KPIcard
-
 title="WO Open"
-
 value="12"
-
 color="text-red-400"
-
 />
 
+<KPIcard
+title="Breakdown"
+value="5"
+unit="cases"
+color="text-orange-500"
+/>
+
+<KPIcard
+title="Downtime"
+value="320"
+unit="min"
+color="text-red-500"
+/>
+
+<KPIcard
+title="MTTR"
+value="1.5"
+unit="hrs"
+/>
+
+<KPIcard
+title="MTBF"
+value="140"
+unit="hrs"
+color="text-green-400"
+/>
 
 </div>
-
 
 </div>
 
